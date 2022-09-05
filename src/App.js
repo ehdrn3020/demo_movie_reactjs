f6d14a76a0051ae26e88dacb6baac2d37266fd8b
ehdrn3020 <ehdrnsla3020@naver.com>
2022-09-06 08:08:55 +0900

    use Props and import css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import Button from "./Buttom";
+import styles from "./App.module.css";
 
 function App() {
   return (
-    <div >
-      <h1>welcome back</h1>
+    <div>
+      <h1 className={styles.title}>welcome back</h1>
+      <Button text={"Continue"} />
     </div>
   );
 }
