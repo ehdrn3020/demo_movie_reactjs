06fb2288ebeee8274621914637906965872b23af
ehdrn3020 <ehdrnsla3020@naver.com>
2022-09-14 08:35:48 +0900

    20220914 add todos

--- src/App.js
+++ src/App.js
@@ -3,37 +3,27 @@ import styles from "./App.module.css";
 import { useState, useEffect } from "react";
 
 function App() {
-  const [counter, setCounter] = useState(0);
-  const [keyword, setKeyword] = useState("");
-
-  const onClick = () => { setCounter((prev) => prev + 1); }
-  const onChange = (event) => {
-    setKeyword(event.target.value);
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    console.log(toDo);
+    if (toDo === "") {
+      return;
+    }
+    setToDo("");
+    setToDos((currentArray) => [toDo, ...currentArray]);
   };
-
-  console.log("i run all the time");
-  const runOnce = () => { console.log("i run once"); };
-  const runOnlyKeyword = () => { 
-    if (keyword !== "") console.log("search for", keyword); 
-  };
-
-  // 첫번째 Render일 때만 실행하게끔
-  useEffect(runOnce, []);
-  // 특정 state가 변할 때만 실행하게끔
-  useEffect(runOnlyKeyword, [keyword]);
-  // 복수 맵핑
-  useEffect(() => { console.log("run keyword & counter"); }, [keyword, counter]);
+  console.log(toDos);
 
   return (
     <div>
-      <input
-        onChange={onChange}
-        value={keyword}
-        type="text"
-        placeholder="Search Here"
-      />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>Click me</button>
+      <h1>My To Dos [{toDos.length}]</h1>
+      <form onSubmit={onSubmit}>
+        <input onChange={onChange} value={toDo} type="text" placeholder="Write your to do"/>
+        <button>Add To Do</button>
+      </form>
     </div>
   );
 }
